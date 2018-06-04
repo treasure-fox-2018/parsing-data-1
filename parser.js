7b5e7e5a4d58c97ab113fa451b1efd13a87a10ee
ronifajrian <ronifajrian@gmail.com>
2018-06-04 17:02:58 +0700

    parsing-1

--- parser.js
+++ parser.js
@@ -3,6 +3,14 @@
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(arrInfo){
+    this.id = arrInfo[0]
+    this.first_name = arrInfo[1]
+    this.last_name = arrInfo[2]
+    this.email = arrInfo[3]
+    this.phone = arrInfo[4]
+  }
+
 }
 
 class PersonParser {
@@ -12,14 +20,42 @@ class PersonParser {
     this._people = null
   }
 
-  get people() {
+  get people() { 
+    
+    return this.parsing()
+  }
+  get file() { 
+    
+    return this._file
+  }
+
+  parsing() {
+    const fs = require('fs')
+    this._people = fs.readFileSync(this._file).toString().split('\n')
+
+    var arrPeople = []
+    for (let i = 1; i < this._people.length; i++) {
+      this._people[i] = this._people[i].split(',')
+         
+      
+      var objPerson = new Person(this._people[i])
+
+      arrPeople.push(objPerson)
+
+    }
+
+    this._people = arrPeople
     return this._people
   }
 
-  addPerson() {}
+  addPeople(){}
+
 
 }
 
 let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// console.log(parser.addPerson());
+
+
+console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
