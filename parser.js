817ceef4bf7ce3aed97b8130eb7e07e0ad195765
sumarsanaadi <sumarsana.adi@gmail.com>
2018-06-04 18:52:25 +0700

    parser release 0 1 2 - sumarsanaadi

--- parser.js
+++ parser.js
@@ -1,8 +1,17 @@
 "use strict"
 
+let fs = require('fs');
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(personDataArr) {
+    this.id = personDataArr[0];
+    this.first_name = personDataArr[1];
+    this.last_name = personDataArr[2];
+    this.email = personDataArr[3];
+    this.phone = personDataArr[4];
+    //RELEASE 2
+    this.created_at = new Date(personDataArr[5]);
+  }
 }
 
 class PersonParser {
@@ -13,13 +22,61 @@ class PersonParser {
   }
 
   get people() {
-    return this._people
+    return ( {
+      person : this._people,
+      length : this._people.length
+    })
   }
 
-  addPerson() {}
+  get file() {
+    return this._file;
+  }
+
+  parse() {
+    let fs = require('fs');
+    let arrayPeople = fs.readFileSync(this._file).toString().split("\n");
+    arrayPeople.shift();
+    let arrayObjPeople = [];
+    for (let i = 0 ; i < arrayPeople.length ; i++) {
+      let person = new Person (arrayPeople[i].split(","));
+      arrayObjPeople.push(person);
+    }
+    this._people = arrayObjPeople;
+    return this._people;
+  }
+
+  addPerson(objPerson) {
+    this._people.push(objPerson);
+  }
+  
+  save() {
+    let stringOutput = "";
+    let lineString = [];
+    stringOutput += Object.keys(this._people[0]).join(",");
+    for (let i = 0 ; i < this._people.length; i++) {
+      for (let key in this._people[i]) {
+        lineString.push(this._people[i][key])
+      }
+      stringOutput += "\n"+lineString.join(",");
+      lineString = [];
+    }
+    fs.writeFileSync(this._file, stringOutput)
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+
+//RELEASE 0
+console.log(parser.parse());
+// console.log(parser.parse());
+console.log("")
+
+//RELEASE 1
+let newPersonID = parser.people.length+1;
+let newPerson = [`${newPersonID}`,'Fatima', 'Staff','tesa@euplacerateget.ca','1-459-215-5529','2012-11-23T07:04:40-08:00'];
+parser.addPerson(new Person(newPerson));
+parser.save();
+
+console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
