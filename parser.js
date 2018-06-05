1c8e6fcdc7c90febe2a3e7de995e9f93244464e3
Hacktiv8 <hacktiv8@H8JKT002.localdomain>
2018-06-05 16:20:27 +0700

    refactoring

--- parser.js
+++ parser.js
@@ -27,12 +27,11 @@ class PersonParser {
     var peopleObjArr = []
 
     for (let i = 1; i < peopleArr.length; i++){
-      peopleObjArr.push(peopleArr[i].split(','))
+      let param = peopleArr[i].split(',')
+      let person = new Person (param[0], param[1], param[2], param[3], param[4], param[5])
+      peopleObjArr.push(person)
     }
-    for (let i in peopleObjArr){
-      this._people.push(new Person (peopleObjArr[i][0], peopleObjArr[i][1], peopleObjArr[i][2], peopleObjArr[i][3], peopleObjArr[i][4], peopleObjArr[i][5]))
-    }
-    return this._people
+    return peopleObjArr
   }
 
   get people() {
@@ -61,10 +60,10 @@ class PersonParser {
 
 let parser = new PersonParser('people.csv')
 
+console.log(parser.parseData())
+// console.log((fs.readFileSync('people.csv').toString().split('\n')[0]).split(','))
 
-// console.log(parser.parseData())
-// console.log(parser.people())
 
 let Brian = new Person (9999, 'Brian', 'Fury', 'g@mail.com', '081234567890', '2018-06-04T07:04:40-08:00')
-console.log(parser.addPerson(Brian))
+// console.log(parser.addPerson(Brian))
 // console.log(`There are ${parser._size} people in the file '${parser._file}'.`)
